102154d265401d55fd22121255bffed769e2446b
FirozTheProgrammer <firozhasan1542@gmail.com>
2024-07-09 17:10:01 +0600

    added download cv button in home page and some bugs fixed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Container from "../../Layout/Container";
 import image from "../../assets/images/firoz-dp.png";
+import resume from "../../assets/firoz-resume.pdf";
 import "./home.css";
 import CountUp from "react-countup";
 import {
@@ -16,50 +17,60 @@ const Home = () => {
       <div>
         <div className="flex  flex-col-reverse lg:flex-row justify-between lg:my-20 font-Poppins items-center align-middle  lg:space-x-10 px-5">
           <div className=" space-y-5 pt-5 text-center lg:text-left">
-            <div className="space-y-2 font-Baskerville">
+            <div className="space-y-2 font-Baskerville text-gray-300">
               <p className="text-sm md:text-xl md:font-bold">Web Developer</p>
               <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold">
                 Hello I&apos;m
               </h1>
               <h2
-                className="text-3xl md:text-5xl lg:text-7xl text-green-400 tracking-wide
+                className="text-5xl lg:text-7xl text-green-400 tracking-wide
  font-bold "
               >
                 Firoz Al Hasan
               </h2>
             </div>
             <p
-              className=" md:w-96 text-gray-500 tracking-wide
+              className=" md:w-96  tracking-wide
 "
             >
               I am a Web Developer at heart and create features that are best
               suited for the job at hand.
             </p>
-            <ul className="flex justify-center lg:justify-start space-x-4 text-xl md:text-2xl">
-              <li>
-                <Link to={"https://github.com/FirozTheProgrammer"}>
-                  <FaGithub></FaGithub>
-                </Link>
-              </li>
-              <li>
-                <Link to={"https://www.facebook.com/FirozTheProgrammer/"}>
-                  <FaFacebook></FaFacebook>
-                </Link>
-              </li>
-              <li>
-                <Link to={"https://twitter.com/Developer_Firoz"}>
-                  <FaSquareXTwitter />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  target="blank"
-                  to={"https://www.linkedin.com/in/firoz-al-hasan/"}
+            <div className="flex flex-col md:flex-row justify-center md:items-center md:space-x-4">
+              <a href={resume} download="resume">
+                <button
+                  type="button"
+                  className="bg-[#EBE9FC] duration-300 hover:bg-green-400 text-black font-bold rounded-md my-5 px-4 py-2"
                 >
-                  <FaLinkedin></FaLinkedin>
-                </Link>
-              </li>
-            </ul>
+                  Download CV
+                </button>
+              </a>
+              <ul className="flex justify-center lg:justify-start space-x-4 text-3xl">
+                <li>
+                  <Link to={"https://github.com/FirozTheProgrammer"}>
+                    <FaGithub></FaGithub>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"https://www.facebook.com/FirozTheProgrammer/"}>
+                    <FaFacebook></FaFacebook>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"https://twitter.com/Developer_Firoz"}>
+                    <FaSquareXTwitter />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    target="blank"
+                    to={"https://www.linkedin.com/in/firoz-al-hasan/"}
+                  >
+                    <FaLinkedin></FaLinkedin>
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
 
           <div className="box my-8 lg:my-0">
@@ -71,7 +82,7 @@ const Home = () => {
           </div>
         </div>
 
-        <div className=" flex flex-col lg:flex-row  items-center space-y-5 lg:space-y-0  lg:justify-between md:px-5 py-5 lg:py-0 -ml-20 lg:-ml-0">
+        <div className=" flex flex-col lg:flex-row  items-center space-y-5 lg:space-y-0  lg:justify-between md:px-5 py-5 my-8 lg:my-0 lg:py-0 -ml-20 lg:-ml-0">
           <div className="w-10 flex space-x-2">
             <p className="text-5xl font-semibold font-Poppins">
               {" "}
